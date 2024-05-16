6b7314f432f33dc58dcf1208682d31e6732578c5
lucasjpl1 <126540649+lucasjpl1@users.noreply.github.com>
2024-05-16 17:37:27 -0300

    falta pouco

--- expedicao-main/public/atualizar_lotes.php
+++ expedicao-main/public/atualizar_lotes.php
@@ -20,5 +20,13 @@ if ($conn->query($sql) === TRUE) {
     echo "Erro ao atualizar os códigos: " . $conn->error;
 }
 
+// Executar o SQL para atualizar os códigos associando-os ao lote recém-criado
+$sql = "UPDATE Devolucao SET Lote = (SELECT MAX(id) FROM lotes) WHERE Lote IS NULL";
+if ($conn->query($sql) === TRUE) {
+    http_response_code(200); // Responder com o código de sucesso 200
+} else {
+    echo "Erro ao atualizar os códigos: " . $conn->error;
+}
+
 $conn->close();
 ?>
